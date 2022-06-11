5d775c11c4e50eacd06fc78b17c21814e8e66dbb
rajeshitor <rajeshcpi788@gmail.com>
2022-06-12 00:35:56 +0600

    1st commit

---
+++ Reverse_Name.py
@@ -0,0 +1,8 @@
+First_Name=input("Enter First Name : ")
+Last_Name=input("Enter First Name : ")
+print("Hello",First_Name,Last_Name)
+
+print("After Reverse")
+
+
+print("Hello",Last_Name,First_Name)
